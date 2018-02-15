a6ab2652992e74d490ee32ede88e86b1c8164526
Jack Sea <jack.sidorenko@hotmail.com>
2018-02-15 02:01:20 -0800

    new colorizer... seems to be working

--- vs_code_extension/story_script/test storyscript project/example.ts
+++ vs_code_extension/story_script/test storyscript project/example.ts
@@ -1,3 +1,28 @@
-const foo = 'foo-asdasdasd'
-
-const boobar = `${foo}bar`;
+const ast = {
+  "Новая Структура.стс": {
+    type: "object",
+    members: {
+      "Утилиты": {
+        type: "object",
+        members: {
+          "Без имени": {
+            type: "string",
+            value: "Без имени"
+          },
+          "Конкат": {
+            type: "function",
+            params: {
+              type: "object",
+              members: {
+                "префикс": {
+                  type: "variable",
+                  
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
